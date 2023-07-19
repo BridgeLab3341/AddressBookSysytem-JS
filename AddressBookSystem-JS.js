8950671fc765c20dc1f574ff86186a62c143e903
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-19 12:34:49 +0530

    [somu]Add:Find Existing Contact person using their name and edit it.

--- AddressBookSystem-JS.js
+++ AddressBookSystem-JS.js
@@ -98,6 +98,16 @@ try{
     contact = new Contact(3,"Radha","Patil","Defg","KoLhapur","Maharashtra","416022","9359915712","radha@gmail.com");
     contactArray.push(contact);
     console.log(contactArray);
+
+    let editName = "Anil";
+    for(let item of contactArray)
+    {
+        if(item._firstname==editName)
+        {
+            item._firstname="Ram";
+        }
+    }
+    console.log(contactArray);
 }
 catch
 {
